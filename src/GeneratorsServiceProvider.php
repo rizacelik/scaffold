98564594498dc66bd65796779e3785a8a78ed04c
rizacelik <rizacelik@gmail.com>
2016-06-26 23:43:11 +0300

    Create GeneratorsServiceProvider.php

---
+++ src/GeneratorsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Scaffold\Builder;
+
+use Illuminate\Support\ServiceProvider;
+class GeneratorsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands('Paketim\Paketismim\Scaffold');
+    }
+}
