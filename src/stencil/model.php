6939ecf9be801ceb579f1bd82465b6fc778fd905
rizacelik <rizacelik@gmail.com>
2016-06-26 23:49:31 +0300

    Create model.php

---
+++ src/stencil/model.php
@@ -0,0 +1,17 @@
+<?php 
+return "<?php
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class $app_name extends Model {
+	
+    $tablename
+	
+    public \$timestamps = false;
+	
+	protected \$fillable = [$fillable];
+$imp
+}
+";
+?>
