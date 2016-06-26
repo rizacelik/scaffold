0c6923c0be6cef1537936ab6dc91614131bc842d
rizacelik <rizacelik@gmail.com>
2016-06-26 23:46:33 +0300

    Create _form.php

---
+++ src/stencil/_form.php
@@ -0,0 +1,28 @@
+<?php 
+return <<<EOT
+@if (count(\$errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach (\$errors->all() as \$error)
+                <li>{{ \$error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+<?php
+  \$method = \$action == 'update' ? '<input type="hidden" name="_method" value="PUT">' : '';
+  \$action = \$action == 'update' ? \$action.'/'.\${$app_var}->{$id} : \$action; 
+?>
+<form action="<?= url('$app_var/'.\$action) ?>" method="POST" class="form-horizontal">
+<?= \$method ?>
+	<fieldset>
+    $forms
+		<div class="form-group">
+			<label class="control-label">&nbsp;</label>
+			<input type="submit" value="Save" class="btn btn-primary">
+		</div>
+	</fieldset>
+	<input type="hidden" name="_token" value="{{ csrf_token() }}">
+</form>
+EOT;
+?>
