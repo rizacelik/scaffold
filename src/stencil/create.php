8eadb7a6ef17d4f0623a35fcc0f00da55a005ece
rizacelik <rizacelik@gmail.com>
2016-06-26 23:48:01 +0300

    Create create.php

---
+++ src/stencil/create.php
@@ -0,0 +1,15 @@
+<?php
+return <<<EOT
+@extends('layouts.app')
+@section('content')
+<div class="container">
+<h2>Create <span class='muted'>${!${''} = ucfirst(str_replace('_', ' ', $key))}</span></h2>
+<hr>
+
+@include('{$app_var}._form', ['action'=> 'store'])
+
+<p><a href="{{url('$app_var')}}">Back</a></p>
+</div>
+@endsection
+EOT;
+?>
